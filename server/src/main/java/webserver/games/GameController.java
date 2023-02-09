996f0a351867fd780c02b269e7f926e20046dd0b
Filippo Venturini <filippo.venturini8@studio.unibo.it>
2023-02-09 11:51:59 +0100

    Add GameAPI doc

--- server/src/main/java/webserver/games/GameController.java
+++ server/src/main/java/webserver/games/GameController.java
@@ -1,5 +1,7 @@
 package webserver.games;
 
+import common.Game;
+import common.Lobby;
 import io.javalin.http.Context;
 import io.javalin.http.HttpResponseException;
 import io.javalin.openapi.*;
@@ -13,12 +15,57 @@ public interface GameController extends Controller {
 
     @OpenApi(
             operationId = "GameApi::startGame",
-            path = HangmanService.BASE_URL + "/games",
+            path = HangmanService.BASE_URL + "/games/{lobbyId}",
             methods = {HttpMethod.POST},
             tags = {"games"},
             description = "Start a new game",
+            pathParams = {
+                    @OpenApiParam(
+                            name = "lobbyId",
+                            type = String.class,
+                            description = "The id of the lobby where to start the game",
+                            required = true
+                    )
+            },
             requestBody = @OpenApiRequestBody(
-                    description = "The creator's nickname",
+                    description = "The new game to put in the lobby",
+                    required = true,
+                    content = {
+                            @OpenApiContent(
+                                    from = Game.class,
+                                    mimeType = ContentType.JSON
+                            )
+                    }
+            ),
+            responses = {
+                    @OpenApiResponse(
+                            status = "200",
+                            description = "The provided id corresponds to a lobby, the game is now started and associated to the lobby, nothing is returned."
+                    ),
+                    @OpenApiResponse(
+                            status = "404",
+                            description = "Not found: the provided id corresponds to not existing lobby"
+                    )
+            }
+    )
+    void postGame(Context context) throws HttpResponseException;
+
+    @OpenApi(
+            operationId = "GameApi::setWordToGuess",
+            path = HangmanService.BASE_URL + "/games/{lobbyId}",
+            methods = {HttpMethod.PUT},
+            tags = {"games"},
+            description = "Set the word to guess, given the id of the game's lobby",
+            pathParams = {
+                    @OpenApiParam(
+                            name = "lobbyId",
+                            type = String.class,
+                            description = "Id of the lobby",
+                            required = true
+                    )
+            },
+            requestBody = @OpenApiRequestBody(
+                    description = "The word to guess",
                     required = true,
                     content = {
                             @OpenApiContent(
@@ -30,7 +77,7 @@ public interface GameController extends Controller {
             responses = {
                     @OpenApiResponse(
                             status = "200",
-                            description = "The nickname provided is correct, the lobby is created and it's id returned.",
+                            description = "The provided id corresponds to a lobby, the word is set, and it's encoded version is returned",
                             content = {
                                     @OpenApiContent(
                                             from = String.class,
@@ -40,16 +87,86 @@ public interface GameController extends Controller {
                     ),
                     @OpenApiResponse(
                             status = "404",
-                            description = "Not found: the provided nickname corresponds to no known user"
+                            description = "Not found: the provided id corresponds to not existing lobby"
                     )
             }
     )
-    void postGame(Context context) throws HttpResponseException;
-
     void putWordToGuess(Context context) throws HttpResponseException;
 
+    @OpenApi(
+            operationId = "GameApi::getGame",
+            path = HangmanService.BASE_URL + "/lobbies/{lobbyId}",
+            methods = {HttpMethod.GET},
+            tags = {"games"},
+            description = "Gets the data of a game, given the corresponding lobby's id.",
+            pathParams = {
+                    @OpenApiParam(
+                            name = "lobbyId",
+                            type = String.class,
+                            description = "Id of the lobby",
+                            required = true
+                    )
+            },
+            responses = {
+                    @OpenApiResponse(
+                            status = "200",
+                            description = "The provided id corresponds to a lobby, the data of it's game are returned",
+                            content = {
+                                    @OpenApiContent(
+                                            from = Game.class,
+                                            mimeType = ContentType.JSON
+                                    )
+                            }
+                    ),
+                    @OpenApiResponse(
+                            status = "404",
+                            description = "Not found: the provided id corresponds to not existing lobby"
+                    )
+            }
+    )
     void getGame(Context context) throws HttpResponseException;
 
+    @OpenApi(
+            operationId = "GameApi::setWordToGuess",
+            path = HangmanService.BASE_URL + "/games/attempt/{lobbyId}",
+            methods = {HttpMethod.PUT},
+            tags = {"games"},
+            description = "Try an attept to guess the word, given the game's lobby id",
+            pathParams = {
+                    @OpenApiParam(
+                            name = "lobbyId",
+                            type = String.class,
+                            description = "Id of the lobby",
+                            required = true
+                    )
+            },
+            requestBody = @OpenApiRequestBody(
+                    description = "The attempt",
+                    required = true,
+                    content = {
+                            @OpenApiContent(
+                                    from = String.class,
+                                    mimeType = ContentType.JSON
+                            )
+                    }
+            ),
+            responses = {
+                    @OpenApiResponse(
+                            status = "200",
+                            description = "The provided id corresponds to a lobby, the attempt is tried and the result of the attempt is returned",
+                            content = {
+                                    @OpenApiContent(
+                                            from = Boolean.class,
+                                            mimeType = ContentType.JSON
+                                    )
+                            }
+                    ),
+                    @OpenApiResponse(
+                            status = "404",
+                            description = "Not found: the provided id corresponds to not existing lobby"
+                    )
+            }
+    )
     void putGuessAttempt(Context context) throws HttpResponseException;
 
     static GameController of(String root) {
